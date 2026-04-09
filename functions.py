9cd38ba1497ce9536d5460af39e6c1b75aadb37d
Othniel <odarlingampofo@gmail.com>
2026-04-09 12:08:42 -0500

    update to functions

--- functions.py
+++ functions.py
@@ -248,4 +248,14 @@
 # usernames = ['john', 'mike','james']
 # greet_users(usernames)
 
-#modifing the list 
+#modifing the list in a function 
+unprinted_design = [ 'phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+while unprinted_design:
+    current_design = unprinted_design.pop()
+    print(f"Prining model: {current_design.title()}")
+    completed_models.append(current_design)
+print("\nThe following models have been printed:")
+for completed_model in completed_models:
+    print(completed_model.title())
