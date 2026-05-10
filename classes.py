43ac73a349e615964d626ded432ad187ccd2c6e8
Othniel <odarlingampofo@gmail.com>
2026-05-10 13:05:21 -0500

    classes

--- classes.py
+++ classes.py
@@ -117,3 +117,20 @@
 
 # my_car = Car("audi", 'a4',2025)
 # print(my_car.get_descriptive_name())
+
+#setting a default value for an attribute 
+class Car:
+    def __init__(self,make,model,year):
+        self.make = make 
+        self.model = model
+        self.year = year 
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name
+    
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} mile on it.")
+
+my_new_car = Car("audi","a4",2024)
